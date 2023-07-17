017eaf66bc48b9338da1e88e2547f92df7de2c09
Adophilus <uchenna19of@gmail.com>
2023-07-17 21:41:11 +0100

    updated layout

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Chibundu's Birthday</title>
+        <meta name="description" content="Chibundu's birthday" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body
+        className="flex min-h-screen"
+        style={{
+          backgroundImage: "url('/star.jpg')",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center",
+          backgroundSize: "cover"
+        }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
