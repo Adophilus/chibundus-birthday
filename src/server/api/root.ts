3d7b105253f1b14ec16a8191d562e767ee3997f7
Adophilus <uchenna19of@gmail.com>
2023-07-23 10:52:01 +0100

    updated router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+import { transferRouter } from "~/server/api/routers/transfer";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  transfer: transferRouter,
 });
 
 // export type definition of API
