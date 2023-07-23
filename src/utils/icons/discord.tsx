a22af88d86434810cdaf6d9e0bbe92d3587ba5cb
Adophilus <uchenna19of@gmail.com>
2023-07-23 10:51:54 +0100

    updated discord icon

--- src/utils/icons/discord.tsx
+++ src/utils/icons/discord.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react"
+import type { FunctionComponent } from "react"
 
 const DiscordIcon: FunctionComponent<{ className: string }> = ({ className }) => {
   return (
