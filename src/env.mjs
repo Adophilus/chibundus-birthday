30706180c29e6ae0ae893fd8ac63a539d9a2d8e4
Adophilus <uchenna19of@gmail.com>
2023-07-24 14:28:24 +0100

    added environment variables

--- src/env.mjs
+++ src/env.mjs
@@ -1,5 +1,5 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
 
 export const env = createEnv({
   /**
@@ -23,9 +23,14 @@ export const env = createEnv({
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
+    DISCORD_BEARER_TOKEN: z.string(),
     TWITTER_CLIENT_ID: z.string(),
     TWITTER_CLIENT_SECRET: z.string(),
-    TWITTER_BEARER_TOKEN: z.string()
+    TWITTER_BEARER_TOKEN: z.string(),
+    POLYGON_MAINNET_RPC_URL: z.string().url(),
+    POLYGON_TESTNET_RPC_URL: z.string().url(),
+    POLYGON_WALLET_PRIVATE_KEY: z.string(),
+    POLYGON_MAINNET_GIFT_MATIC_AMOUNT: z.coerce.number()
   },
 
   /**
@@ -35,6 +40,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    NEXT_PUBLIC_DISCORD_SERVER_URL: z.string()
   },
 
   /**
@@ -48,13 +54,20 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    DISCORD_BEARER_TOKEN: process.env.DISCORD_BEARER_TOKEN,
     TWITTER_CLIENT_ID: process.env.TWITTER_CLIENT_ID,
     TWITTER_CLIENT_SECRET: process.env.TWITTER_CLIENT_SECRET,
-    TWITTER_BEARER_TOKEN: process.env.TWITTER_BEARER_TOKEN
+    TWITTER_BEARER_TOKEN: process.env.TWITTER_BEARER_TOKEN,
+    NEXT_PUBLIC_DISCORD_SERVER_URL: process.env.NEXT_PUBLIC_DISCORD_SERVER_URL,
+    POLYGON_MAINNET_RPC_URL: process.env.POLYGON_MAINNET_RPC_URL,
+    POLYGON_TESTNET_RPC_URL: process.env.POLYGON_TESTNET_RPC_URL,
+    POLYGON_WALLET_PRIVATE_KEY: process.env.POLYGON_WALLET_PRIVATE_KEY,
+    POLYGON_MAINNET_GIFT_MATIC_AMOUNT:
+      process.env.POLYGON_MAINNET_GIFT_MATIC_AMOUNT
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
    * This is especially useful for Docker builds.
    */
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-});
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION
+})
