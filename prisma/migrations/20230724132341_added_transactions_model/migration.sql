c38be8be90b890e29195cf680153351054c63604
Adophilus <uchenna19of@gmail.com>
2023-07-24 14:27:56 +0100

    added prisma migration

---
+++ prisma/migrations/20230724132341_added_transactions_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "recipientAddress" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
