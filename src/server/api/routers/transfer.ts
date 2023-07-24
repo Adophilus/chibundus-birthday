eeab71ea41d998431e046496de0b021f9a4644b7
Adophilus <uchenna19of@gmail.com>
2023-07-24 14:28:05 +0100

    updated the transfer router

--- src/server/api/routers/transfer.ts
+++ src/server/api/routers/transfer.ts
@@ -1,30 +1,50 @@
-import { Client } from "twitter-api-sdk";
-import { z } from "zod";
-import { env } from "~/env.mjs";
-import {
-  createTRPCRouter,
-  publicProcedure,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { TRPCError } from "@trpc/server"
+import axios from "axios"
+import { z } from "zod"
+import { env } from "~/env.mjs"
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc"
+import { sendMatic } from "~/utils/web3"
 
 export const transferRouter = createTRPCRouter({
-  hello: publicProcedure
-    .query(async ({ ctx }) => {
-      try {
-        // console.log(ctx.session);
-        return { message: "success" };
-      }
-      catch (err) {
-        console.log(err)
-        return { error: "something bad happened" }
-      }
+  sendMatic: publicProcedure
+    .input(
+      z.object({
+        recipientAddress: z.string()
+      })
+    )
+    .query(async ({ input }) => {
+      return await sendMatic({ recipientAddress: input.recipientAddress })
     }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+  checkDiscord: publicProcedure.query(async () => {
+    try {
+      const res = await axios.get(
+        "https://discord.com/api/v10/users/@me/guilds",
+        {
+          params: {
+            limit: 10
+          },
+          headers: {
+            Authorization: `Bot ${env.DISCORD_BEARER_TOKEN}`
+          }
+        }
+      )
+      console.log(res.data)
+      return res.data
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        if (!err.response)
+          throw new TRPCError({
+            message: "Couldn't connect to Discord API",
+            code: "INTERNAL_SERVER_ERROR"
+          })
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
-});
+        console.log(err.response?.data)
+      }
+      throw new TRPCError({
+        message: "An error occurred",
+        code: "INTERNAL_SERVER_ERROR"
+      })
+    }
+  })
+})
