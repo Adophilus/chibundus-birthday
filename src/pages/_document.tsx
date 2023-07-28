28762395ee6322f4a3d0bb6944481b716052a805
Adophilus <uchenna19of@gmail.com>
2023-07-28 21:41:43 +0100

    updated the title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Chibundu's Birthday</title>
+        <title>NovaRetro</title>
         <meta name="description" content="Chibundu's birthday" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
